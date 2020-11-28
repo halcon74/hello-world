ef43f1435a58b4242c95bee5c5517a16ec6d43c6
halcon74 <halcon@tuta.io>
2020-11-29 02:12:31 +0300

    and one more comment edit

--- helpers.py
+++ helpers.py
@@ -50,7 +50,9 @@ because I define OS here as a set of variables, for:
 
 - making similar installs on similar Linux distributions derived
 from one distro;
-- giving the user full freedom to simulate another OS if he wants to
+
+- giving the user full freedom to simulate another OS if he wants to;
+
 - setting custom hooks in any class function for any OS as simple as
 if int_data['detected_os'] == 'gentoo':
 if int_data['detected_os'] == 'debian':
